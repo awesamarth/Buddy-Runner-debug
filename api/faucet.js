8c1243ce1ba7ac4eb5b6644ff90da2592db6bf52
Samarth Saxena <samarthsaxena1672003@gmail.com>
2025-10-04 22:43:11 +0530

    testing

--- api/faucet.js
+++ api/faucet.js
@@ -29,6 +29,8 @@ const NETWORKS = {
 };
 
 export default async function handler(req, res) {
+
+  console.log("faucet called")
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
